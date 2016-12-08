507dcc2b4668582f65a822678dcb8db3b232dd7e
Alexey Popovich <gorynychalex@gmail.com>
2016-12-08 17:10:23 +1000

    Комментарии

--- src/ru/dvfu/mrcpk/ex15database/sql.java
+++ src/ru/dvfu/mrcpk/ex15database/sql.java
@@ -4,20 +4,30 @@ import java.sql.*;
 
 public class sql {
     public static void main(String[] args) {
+
+        //Объявления переменных для создания соединения и получения результатов
         Connection connection = null;
         ResultSet resultSet = null;
         Statement statement = null;
 
         try{
+            //Запрос класса по имени
             Class.forName("org.sqlite.JDBC");
+            //Установление соединения
             connection = DriverManager.getConnection("jdbc:sqlite:C:\\test\\car.s3db");
+            //Создание экземпляра для выполнения запросов
             statement = connection.createStatement();
+            //Запрос данных
             resultSet = statement.executeQuery("select * from car");
+
+            //Получение количества полей
             int n = resultSet.getMetaData().getColumnCount();
             for (int i = 1; i <= n; i++) {
                 System.out.print(resultSet.getMetaData().getColumnLabel(i) + "\t");
             }
             System.out.println();
+
+            //Выборки данных полей
             while (resultSet.next()){
                 for (int i = 1; i <= n; i++) {
                     System.out.print(resultSet.getString(i) + "\t");
