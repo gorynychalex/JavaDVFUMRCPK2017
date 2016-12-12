d74cad206d12936dde6c2e55ddf4ffaa076b0644
Alexey Popovich <gorynychalex@gmail.com>
2016-12-12 16:30:10 +1000

    sqlite

--- src/ru/dvfu/mrcpk/ex15database/sql.java
+++ src/ru/dvfu/mrcpk/ex15database/sql.java
@@ -14,26 +14,47 @@ public class sql {
             //Запрос класса по имени
             Class.forName("org.sqlite.JDBC");
             //Установление соединения
-            connection = DriverManager.getConnection("jdbc:sqlite:C:\\test\\car.s3db");
-            //Создание экземпляра для выполнения запросов
+            connection = DriverManager.getConnection("jdbc:sqlite:/home/gorynych/IdeaProjects/JavaDVFUMRCPK201612/src/ru/dvfu/mrcpk/ex15database/car.s3db");
+//            connection = DriverManager.getConnection("jdbc:sqlite:C:\\test\\car.s3db");
+
+            //Создание экземпляра для выполнения запросов (без параметров)
             statement = connection.createStatement();
-            //Запрос данных
+
+
+            //1.Удаление всех данных из базы
+            statement.executeUpdate("DELETE FROM CAR WHERE id >= 0");
+
+            //2. Вставка данных в базу
+            statement.executeUpdate("INSERT INTO car values (1,'TOYOTA')");
+            statement.executeUpdate("INSERT INTO car values (2,'NISSAN')");
+            statement.executeUpdate("INSERT INTO car values (3,'SUBARY')");
+
+            //3. Запорос на выборку данных
             resultSet = statement.executeQuery("select * from car");
 
-            //Получение количества полей
+            //3.1. Получение количества полей
             int n = resultSet.getMetaData().getColumnCount();
+            System.out.println("количество полей: " + n);
             for (int i = 1; i <= n; i++) {
                 System.out.print(resultSet.getMetaData().getColumnLabel(i) + "\t");
             }
             System.out.println();
 
-            //Выборки данных полей
+
+            //3.2. Получение значений:
+            //а) Сначала выбираются записи (строки), пока они есть
+            //б) Выбираются значения по полям (стобцам)
             while (resultSet.next()){
                 for (int i = 1; i <= n; i++) {
                     System.out.print(resultSet.getString(i) + "\t");
                 }
                 System.out.println();
             }
+
+
+            // 4. Обновление записей
+            statement.executeUpdate("update car set name='SUZUKI' where id=3");
+
         } catch (ClassNotFoundException e) {
             e.printStackTrace();
         } catch (SQLException e) {
