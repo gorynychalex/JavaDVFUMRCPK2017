0b7e8e5ff67d848db6d6b6568e10d8ec49bab695
gorynychalex <gorynychalex@gmail.com>
2017-10-26 19:13:01 +1000

    Add files via upload

---
+++ src/ru/dvfu/mrcpk/ex01Begin/Hello.java
@@ -0,0 +1,12 @@
+public class Hello {
+	public static void main( String args[] )
+	{
+		System.out.println("Hello World!");
+		System.out.println("My arguments:");
+		for (int i = 0; i < args.length; i++)
+		{
+		   System.out.println(" arg["+i+"] :"+args[i]);
+		}
+		System.out.println("Good bye!");
+	}
+}
