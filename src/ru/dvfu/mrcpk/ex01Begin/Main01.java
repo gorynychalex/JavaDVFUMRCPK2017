b9a8fb943c334876365b342716366c4e0d9fa26a
popovich.ab <gorynychalex@gmail.com>
2017-10-28 11:45:00 +1000

    Main01

---
+++ src/ru/dvfu/mrcpk/ex01Begin/Main01.java
@@ -0,0 +1,16 @@
+package ru.dvfu.mrcpk.ex01Begin;
+
+//Задача нахождения младшего разряда в числе
+
+import java.util.Scanner;
+
+public class Main01 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int x = scanner.nextInt();
+
+        System.out.println(x%10);
+
+    }
+}
